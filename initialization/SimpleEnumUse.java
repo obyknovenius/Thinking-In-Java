57f4732ad33496af1f87f239d7e4467d60ed6cf4
Vitaly Dyachkov <obyknovenius@me.com>
2015-01-07 14:14:37 +0300

    initialization/SimpleEnumUse

---
+++ initialization/SimpleEnumUse.java
@@ -0,0 +1,8 @@
+// initialization/SimpleEnumUse.java
+
+public class SimpleEnumUse {
+    public static void main(String[] args) {
+        Spiciness howHot = Spiciness.MEDIUM;
+        System.out.println(howHot);
+    }
+}
