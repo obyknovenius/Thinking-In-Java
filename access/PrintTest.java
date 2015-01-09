021f7df4dfe28bd14bf4cbefd15d556ebdccd06e
Vitaly Dyachkov <obyknovenius@me.com>
2015-01-09 15:50:25 +0300

    access/PrintTest

---
+++ access/PrintTest.java
@@ -0,0 +1,12 @@
+// access/PrintTest.java
+// Uses the static printing methods in Print.java.
+import static net.mindview.util.Print.*;
+
+public class PrintTest {
+    public static void main(String[] args) {
+        print("Available from now on!");
+        print("100");
+        print(100L);
+        print(3.13159);
+    }
+}
