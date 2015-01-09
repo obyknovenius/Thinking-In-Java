c24a5365648185aa029b3f821c56a298a146336b
Vitaly Dyachkov <obyknovenius@me.com>
2015-01-09 17:49:48 +0300

    access/Dinner

---
+++ access/Dinner.java
@@ -0,0 +1,10 @@
+// access/Dinner.java
+// Uses the library.
+import access.dessert.*;
+
+public class Dinner {
+    public static void main(String[] args) {
+        Cookie x = new Cookie();
+        // x.bite(); // Can't access
+    }
+}
