d32119c151ff147b3c1c2e85ec7ccdae1f06c17a
Vitaly Dyachkov <obyknovenius@me.com>
2015-02-15 15:20:16 +0300

    polymorphism/shape/Circle

---
+++ polymorphism/shape/Circle.java
@@ -0,0 +1,7 @@
+// polymorphism/shape/Circle.java
+package polymorphism.shape;
+
+public class Circle extends Shape {
+	public void draw() { System.out.println("Circle.draw()"); }
+	public void erase() { System.out.println("Circle.erase()"); }
+}
