7e5cb070fe07ba916a2224d5019fbea8b9140063
Vitaly Dyachkov <obyknovenius@me.com>
2015-02-15 15:23:32 +0300

    polymorphism/shape/Square

---
+++ polymorphism/shape/Square.java
@@ -0,0 +1,7 @@
+// polymorphism/shape/Square.java
+package polymorphism.shape;
+
+public class Square extends Shape {
+	public void draw() { System.out.println("Square.draw()"); }
+	public void erase() { System.out.println("Square.erase()"); }
+}
