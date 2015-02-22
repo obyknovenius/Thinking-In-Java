7285953c22469e0391d31cc87cb3f583e0ed6f68
Vitaly Dyachkov <obyknovenius@me.com>
2015-02-22 21:53:32 +0300

    polymorphism/PolyConstructors

---
+++ polymorphism/PolyConstructors.java
@@ -0,0 +1,29 @@
+// polymorphism/PolyConstructors.java
+// Constructors and polymorphism
+// don't produce what you might expect.
+
+class Glyph {
+	void draw() { System.out.println("Gliph.draw()"); }
+	Glyph() {
+		System.out.println("Glyph() before draw()");
+		draw();
+		System.out.println("Glyph() after draw()");
+	}
+}
+
+class RoundGlyph extends Glyph {
+	private int radius = 1;
+	RoundGlyph(int r) {
+		radius = r;
+		System.out.println("RoundGlyph.RoundGlyph(), radius = " + radius);
+	}
+	void draw() {
+		System.out.println("RoundGlyph.draw(), radius = " + radius);
+	}
+}
+
+public class PolyConstructors {
+	public static void main(String[] args) {
+		new RoundGlyph(5);
+	}
+}
