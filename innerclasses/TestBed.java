da3e35c59ad0e016d306bab6ed1ecbfbd70f0d2f
Vitaly Dyachkov <obyknovenius@me.com>
2015-03-29 15:49:25 +0300

    innerclasses/TestBed

---
+++ innerclasses/TestBed.java
@@ -0,0 +1,12 @@
+// innerclasses/TestBed.java
+// Putting test code in a nested class.
+
+public class TestBed {
+	public void f() { System.out.println("f()"); }
+	public static class Tester {
+		public static void main(String[] args) {
+			TestBed t = new TestBed();
+			t.f();
+		}
+	}
+}
