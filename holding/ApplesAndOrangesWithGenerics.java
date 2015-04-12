50a8c8eaf3360b36e604a8e84e81536ec86f7de6
Vitaly Dyachkov <obyknovenius@me.com>
2015-04-12 20:02:18 +0300

    holding/ApplesAndOrangesWithGenerics

---
+++ holding/ApplesAndOrangesWithGenerics.java
@@ -0,0 +1,16 @@
+// holding/ApplesAndOrangesWithGenerics.java
+import java.util.*;
+
+public class ApplesAndOrangesWithGenerics {
+	public static void main(String[] args) {
+		ArrayList<Apple> apples = new ArrayList<Apple>();
+		for (int i = 0; i < 3; i++)
+			apples.add(new Apple());
+		// apples.add(new Orange());
+		for (int i = 0; i < apples.size(); i++)
+			System.out.println(apples.get(i).id());
+		// Using foreach:
+		for (Apple c : apples)
+			System.out.println(c.id());
+	}
+}
