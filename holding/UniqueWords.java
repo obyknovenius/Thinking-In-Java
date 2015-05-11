1b5a14dad099f8db916683a8d6704d92783e6ab3
Vitaly Dyachkov <obyknovenius@me.com>
2015-05-11 16:19:10 +0300

    holding/UniqueWords

---
+++ holding/UniqueWords.java
@@ -0,0 +1,11 @@
+// holding/UniqueWords.java
+import java.util.*;
+import net.mindview.util.*;
+
+public class UniqueWords {
+	public static void main(String[] args) {
+		Set<String> words = new TreeSet<String>(
+			new TextFile("SetOperations.java", "\\W+"));
+		System.out.println(words);
+	}
+}
