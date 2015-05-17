3d1c1ba7c3cb3f8831086a3675dcbee73b290a47
Vitaly Dyachkov <obyknovenius@me.com>
2015-05-17 16:18:16 +0300

    holding/NonCollectionSequence

---
+++ holding/NonCollectionSequence.java
@@ -0,0 +1,26 @@
+// holding/NonCollectionSequence.java
+import typeinfo.pets.*;
+import java.util.*;
+
+class PetSequence {
+	protected Pet[] pets = Pets.createArray(8);
+}
+
+public class NonCollectionSequence extends PetSequence {
+	public Iterator<Pet> iterator() {
+		return new Iterator<Pet>() {
+			private int index = 0;
+			public boolean hasNext() {
+				return index < pets.length();
+			}
+			public Pet next() { return pets[index++]; }
+			public void remove() { // Not implemented
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
+	public static void main(String[] args) {
+		NonCollectionSequence nc = new NonCollectionSequence();
+		InteraceVsIterator.display(nc.iterator());
+	}
+}
