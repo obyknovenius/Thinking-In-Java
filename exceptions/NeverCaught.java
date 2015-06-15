85d2a0600a40e52063fe730b93e63dbc9d618538
Vitaly Dyachkov <obyknovenius@me.com>
2015-06-15 21:55:18 +0300

    exceptions/NeverCaught

---
+++ exceptions/NeverCaught.java
@@ -0,0 +1,14 @@
+// exceptions/NeverCaught.java
+// Ignoring RuntimeExceptions.
+
+public class NeverCaught {
+	static void f() {
+		throw new RuntimeException("From f()");
+	}
+	static void g() {
+		f();
+	}
+	public static void main(String[] args) {
+		g();
+	}
+}
