f340390221eafaa1dda07448f0284cb1a38aa0e3
Vitaly Dyachkov <obyknovenius@me.com>
2015-06-17 23:12:43 +0300

    exceptions/LostMessage

---
+++ exceptions/LostMessage.java
@@ -0,0 +1,35 @@
+// exceptions/LostMessage.java
+// How an exception can be lost.
+
+class VeryImportantException extends Exception {
+	public String toString() {
+		return "A very important exception!";
+	}
+}
+
+class HoNumException extends Exception {
+	public String toString() {
+		return "A trivial exception";
+	}
+}
+
+public class LostMessage {
+	void f() throws VeryImportantException {
+		throw new VeryImportantException();
+	}
+	void dispose() throws HoNumException {
+		throw new HoNumException();
+	}
+	public static void main(String[] args) {
+		try {
+			LostMessage lm = new LostMessage();
+			try {
+				lm.f();
+			} finally {
+				lm.dispose();
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
